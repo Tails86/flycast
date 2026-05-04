2415e00e04add2b89f3669af877e520fe595ff8b
nexus382 <81545086+nexus382@users.noreply.github.com>
2026-05-04 10:26:58 -0400

    Update core/ui/boxart/boxart.h

--- core/ui/boxart/boxart.h
+++ core/ui/boxart/boxart.h
@@ -32,7 +32,7 @@ struct GameMedia;
 
 enum class BoxartSourceMode
 {
-	ScrapedOnly = 0,
+	ScrapedOnly = 0,  // Only use scraped artwork from the box art database / online scraper
 	PhysicalMediaOnly = 1, // Shows the Physical media image of the disc on the Rom
 	CustomThenScraped = 2,
 };
